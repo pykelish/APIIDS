e87500876c75e7e649e031ec8d62bbe73651762b
Fabian Ramirez <fabianramirez2810@gmail.com>
2023-11-05 20:46:09 -0700

    practica rutas

---
+++ laravel/Primer_Proyecto_Laravel/resources/views/example.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+
+<body>
+
+    <h1>Bienvenido
+        <?= $name ?>
+    </h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, minima exercitationem ut labore
+        perspiciatis ex cumque dicta eum quidem laboriosam beatae fuga pariatur? Dolore amet eos autem neque quisquam
+        ullam!
+    </p>
+</body>
+
+</html>
---
+++ laravel/Primer_Proyecto_Laravel/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/example/{name?}', function ($name = "Fabian") {
+    return view('example',[ 'name' => $name]);
+});
+
+Route::get('/saludo/{nombre}/{lastname?}', function ($nombre, $lastname=null) {
+    return 'Hola ' . $nombre . ' ' . $lastname;
+})->where(['nombre' => '[a-z]+', 'lastname' => '[a-z]+']);
+
+Route::get('/suma/{num1}/{num2}', function ($num1, $num2) {
+    return 'Resultado de la suma es: ' . $num1 + $num2;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+Route::get('/resta/{num1}/{num2}', function ($num1, $num2) {
+    return 'Resultado de la resta es: ' . $num1 - $num2;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+Route::get('/multiplicacion/{num1}/{num2}', function ($num1, $num2) {
+    return 'Resultado de la multiplicacion es: ' . $num1 * $num2;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+Route::get('/division/{num1}/{num2}', function ($num1, $num2) {
+    return 'Resultado de la division es: ' . $num1 / $num2;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+
